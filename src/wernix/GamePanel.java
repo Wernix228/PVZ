634e4268d5df80fa5dfab9e833b1251539a15fd5
Wernix228 <Ktoto2667@gmail.com>
2022-11-11 20:34:22 +0300

    Initial commit

---
+++ src/wernix/GamePanel.java
@@ -0,0 +1,12 @@
+package wernix;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    public GamePanel(int weight, int heigt){
+        setSize(new Dimension(weight,heigt));
+    }
+}
+
---
+++ src/wernix/Main.java
@@ -0,0 +1,9 @@
+package wernix;
+
+public class Main {
+
+    public static void main(String[] args) {
+        GamePanel gamePanel = new GamePanel(500,200);
+
+    }
+}
